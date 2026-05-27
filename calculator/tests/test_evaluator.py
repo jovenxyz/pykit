7013ab3725b8663e9b396ce5381ec4cff4bd4745
jovenxyz <jimmynamba.engineer@gmail.com>
2026-05-27 19:57:44 +1000

    test(evaluator): cover unary, modulo and power

--- calculator/tests/test_evaluator.py
+++ calculator/tests/test_evaluator.py
@@ -11,3 +11,15 @@ def test_basic_arithmetic():
     assert _eval("1 + 2 * 3") == 7
     assert _eval("(1 + 2) * 3") == 9
     assert _eval("10 - 4 - 3") == 3
+
+
+def test_unary_minus():
+    assert _eval("-3 + 5") == 2
+
+
+def test_modulo():
+    assert _eval("7 % 3") == 1
+
+
+def test_power_is_right_associative():
+    assert _eval("2 ** 3 ** 2") == 512
