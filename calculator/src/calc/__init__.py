0e95886ece7d6424bb61ef4f75e6751823b22ce9
jovenxyz <jimmynamba.engineer@gmail.com>
2026-05-27 19:57:45 +1000

    feat(calc): expose evaluate() from the package root

--- calculator/src/calc/__init__.py
+++ calculator/src/calc/__init__.py
@@ -1,3 +1,6 @@
-"""calc: a small arithmetic expression calculator."""
+"""calc: a small, well-tested arithmetic expression calculator."""
+from calc.api import evaluate
 
 __version__ = "0.1.0"
+
+__all__ = ["evaluate"]
