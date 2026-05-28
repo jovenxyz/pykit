663671b4b4db30ec8363a934b9080e839cf72309
jovenxyz <jimmynamba.engineer@gmail.com>
2026-05-28 17:22:21 +1000

    test(cli): cover completing a task

--- todo/tests/test_cli.py
+++ todo/tests/test_cli.py
@@ -12,3 +12,13 @@ def test_add_and_list(tmp_path, capsys):
     assert main(args + ["list"]) == 0
     out = capsys.readouterr().out
     assert "[ ] #1 buy milk" in out
+
+
+def test_done_marks_complete(tmp_path, capsys):
+    args = _file(tmp_path)
+    main(args + ["add", "task a"])
+    capsys.readouterr()
+    assert main(args + ["done", "1"]) == 0
+    capsys.readouterr()
+    main(args + ["list"])
+    assert "[x] #1 task a" in capsys.readouterr().out
